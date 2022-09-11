c34d8165c0400be1faa16a57889b32a6c1bade04
yoonichoi <111607447+yoonichoi@users.noreply.github.com>
2022-09-11 17:34:38 -0400

    Time: 214 ms (14.68%), Space: 16.7 MB (73.75%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        dic = collections.defaultdict(list)
+        larger = 0
+        for num, cnt in collections.Counter(nums).items():
+            dic[cnt].append(num)
+            larger = max(larger, cnt)
+        res = []
+        for freq in range(larger, 0, -1):
+            res.extend(dic[freq])
+            if len(res) >= k:
+                break
+        return res if len(res) == k else res[:k]
