7484164caf54f582a31b85c9a63c8537b1a86ace
yoonichoi <111607447+yoonichoi@users.noreply.github.com>
2022-09-24 11:29:26 -0400

    Time: 11768 ms (81.41%), Space: 120 MB (5.36%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -1,79 +1,46 @@
-class WordNode:
+class TrieNode:
     def __init__(self):
-        self.children = {}
-        self.isEnd = False
-
-class WordDictionary:
-    def __init__(self):
-        self.root = WordNode()
+        self.dict = {}
+        self.word = False
         
+class WordDictionary(object):
+
+    def __init__(self):
+        self.root = TrieNode()
+
     def addWord(self, word):
+        """
+        :type word: str
+        :rtype: None
+        """
         node = self.root
-        for w in word:
-            if w in node.children:
-                node = node.children[w]
+        for c in word:
+            if c in node.dict:
+                node = node.dict[c]
             else:
-                node.children[w] = WordNode()
-                node = node.children[w]
-        node.isEnd = True
-                
+                node.dict[c] = TrieNode()
+                node = node.dict[c]
+        node.word = True
+
     def search(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
         stack = [(self.root,word)]
         while stack:
             node, w = stack.pop()
             if not w:
-                if node.isEnd:
+                if node.word:
                     return True
             elif w[0]=='.':
-                for n in node.children.values():
+                for n in node.dict.values():
                     stack.append((n,w[1:]))
             else:
-                if w[0] in node.children:
-                    n = node.children[w[0]]
+                if w[0] in node.dict:
+                    n = node.dict[w[0]]
                     stack.append((n,w[1:]))
         return False
-# class TrieNode:
-#     def __init__(self):
-#         self.dict = {}
-#         self.word = False
-        
-# class WordDictionary(object):
-
-#     def __init__(self):
-#         self.root = TrieNode()
-
-#     def addWord(self, word):
-#         """
-#         :type word: str
-#         :rtype: None
-#         """
-#         node = self.root
-#         for c in word:
-#             if c in node.dict:
-#                 node = node.dict[c]
-#             else:
-#                 node.dict[c] = TrieNode()
-#                 node = node.dict[c]
-#         node.word = True
-
-#     def search(self, word):
-#         """
-#         :type word: str
-#         :rtype: bool
-#         """
-#         stack = [(self.root, word)]
-#         while stack:
-#             node, w = stack.pop()
-#             if not w and node.word:
-#                 return True
-#             elif w[0] == '.':
-#                 for n in node.dict.values():
-#                     stack.append((n, w[1:]))
-#             else:
-#                 if w[0] in node.dict:
-#                     n = node.dict[w[0]]
-#                     stack.append((n, w[1:]))
-#         return False
 
 
 # Your WordDictionary object will be instantiated and called as such:
