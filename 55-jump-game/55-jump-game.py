7e7fa8fa520b1db4e89e644be6a1c0eb719d23f9
yoonichoi <111607447+yoonichoi@users.noreply.github.com>
2022-09-27 12:54:42 -0400

    Time: 1120 ms (15.72%), Space: 14.5 MB (66.34%) - LeetHub

---
+++ 55-jump-game/55-jump-game.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        length = len(nums)
+        dp = [0]*length
+        dp[0] = nums[0]
+        for i in range(1, length-1):
+            if dp[i-1] < i:
+                return False
+            dp[i] = max(dp[i-1], i+nums[i])
+            if dp[i] >= length-1:
+                return True
+        return dp[length-2] >= length-1
+        
