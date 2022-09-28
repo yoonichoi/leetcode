b9f7a6f341a85a96f30de4b81600f795c301db47
yoonichoi <111607447+yoonichoi@users.noreply.github.com>
2022-09-28 18:23:23 -0400

    Time: 151 ms (12.57%), Space: 17 MB (66.31%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -4,24 +4,23 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: int
         """
-        # start, end, cnt, e = [], [], 0, 0
-        # for i in sorted(intervals):
-        #     start.append(i[0])
-        #     end.append(i[1])
-        # print(start, end, cnt, e)
-        # for s in range(len(start)):
-        #     if start[s] < end[e]:
-        #         cnt += 1
-        #     else:
-        #         e += 1
-        # return cnt
-        e = ret = 0
+        cnt, e = 0, 0
         start = sorted(i[0] for i in intervals)
         end = sorted(i[1] for i in intervals)
-        
+        print(start, end, cnt, e)
         for s in range(len(start)):
-            if start[s] < end[e]: 
-                ret += 1
-            else: 
+            if start[s] < end[e]:
+                cnt += 1
+            else:
                 e += 1
-        return ret
+        return cnt
+#         e = ret = 0
+#         start = sorted(i[0] for i in intervals)
+#         end = sorted(i[1] for i in intervals)
+        
+#         for s in range(len(start)):
+#             if start[s] < end[e]: 
+#                 ret += 1
+#             else: 
+#                 e += 1
+#         return ret
