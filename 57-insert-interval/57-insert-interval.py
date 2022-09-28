8b228ed092ec8785c028a056ccb8745fdc9ecdbf
yoonichoi <111607447+yoonichoi@users.noreply.github.com>
2022-09-28 16:52:35 -0400

    Time: 140 ms (16.54%), Space: 17 MB (20.09%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        s, e = newInterval
+        left, right = [], []
+        for i in intervals:
+            if i[1] < s:
+                left += i,
+            elif i[0] > e:
+                right += i,
+            else:
+                s, e = min(s, i[0]), max(e, i[1])
+        return left + [[s,e]] + right
