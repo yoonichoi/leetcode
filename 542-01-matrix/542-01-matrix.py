c7b7eacf450b1c4f0a21394ebe22dbe9844bf674
yoonichoi <111607447+yoonichoi@users.noreply.github.com>
2022-09-29 23:43:26 -0400

    Time: 1673 ms (11.90%), Space: 16.1 MB (74.69%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def updateMatrix(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: List[List[int]]
+        """
+#         m, n = len(mat), len(mat[0])
+#         q = collections.deque([])
+#         for r in range(m):
+#             for c in range(n):
+#                 if mat[r][c] == 0:
+#                     q.append((r,c))
+#                 else:
+#                     mat[r][c] = -1
+#         while q:
+#             r, c = q.popleft()
+#             for dx, dy in ((0,1),(0,-1),(1,0),(-1,0)):
+#                 nr, nc = r+dx, r+dy
+#                 if nr < 0 or nr==m or nc<0 or nc==n or mat[nr][nc] != -1:
+#                     continue
+#                 mat[nr][nc] = mat[r][c] + 1
+#                 q.append((nr,nc))
+                
+#         return mat
+        m, n = len(mat), len(mat[0])
+        DIR = [0, 1, 0, -1, 0]
+
+        q = deque([])
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c] == 0:
+                    q.append((r, c))
+                else:
+                    mat[r][c] = -1  # Marked as not processed yet!
+
+        while q:
+            r, c = q.popleft()
+            for i in range(4):
+                nr, nc = r + DIR[i], c + DIR[i + 1]
+                if nr < 0 or nr == m or nc < 0 or nc == n or mat[nr][nc] != -1: continue
+                mat[nr][nc] = mat[r][c] + 1
+                q.append((nr, nc))
+        return mat
