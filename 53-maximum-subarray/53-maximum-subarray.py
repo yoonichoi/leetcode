4a1d6aae2343e85c7392581a40c22c64ce56e222
yoonichoi <111607447+yoonichoi@users.noreply.github.com>
2022-09-29 22:13:38 -0400

    Time: 1340 ms (25.70%), Space: 25.9 MB (11.96%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -4,11 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        curmax, allmax = 0, float('-inf')
-        for n in nums:
-            curmax = max(n,curmax+n)
-            allmax = max(curmax,allmax)
-        return allmax
-        
-        
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        for i in range(1,len(nums)):
+            dp[i] = max(dp[i-1]+nums[i], nums[i])
+        return max(dp)
         
