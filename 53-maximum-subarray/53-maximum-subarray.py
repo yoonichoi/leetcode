fd11b3acef8d2c20a18950432665d147377296eb
yoonichoi <111607447+yoonichoi@users.noreply.github.com>
2022-10-01 11:11:27 -0400

    Time: 1927 ms (5.01%), Space: 26.1 MB (5.52%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -4,9 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        dp = [0] * len(nums)
+        if len(nums) == 1: return nums[0]
+        dp = [0] * (len(nums))
         dp[0] = nums[0]
         for i in range(1,len(nums)):
-            dp[i] = max(dp[i-1]+nums[i], nums[i])
+            dp[i] = max(dp[i-1] + nums[i], nums[i])
         return max(dp)
-        
