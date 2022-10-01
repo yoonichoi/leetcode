56655a5ade5352695f24d182ea764866b4473684
yoonichoi <111607447+yoonichoi@users.noreply.github.com>
2022-10-01 11:11:36 -0400

    Time: 1116 ms (41.10%), Space: 26.2 MB (5.52%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: int
         """
         if len(nums) == 1: return nums[0]
-        dp = [0] * (len(nums))
+        dp = [0] * len(nums)
         dp[0] = nums[0]
         for i in range(1,len(nums)):
             dp[i] = max(dp[i-1] + nums[i], nums[i])
