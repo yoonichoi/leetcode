eabfc65d2c4db57e62dd538c27bc5f6ce7862b7c
yoonichoi <111607447+yoonichoi@users.noreply.github.com>
2022-09-30 21:52:27 -0400

    Time: 1724 ms (13.32%), Space: 20.8 MB (9.11%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -5,13 +5,10 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        import heapq
-        heap = []
+        ls = []
         for p in points:
             x, y = p
-            dist = -(x*x+y*y)
-            if len(heap) == k:
-                heapq.heappushpop(heap, (dist, x, y))
-            else:
-                heapq.heappush(heap, (dist, x, y))
-        return [(x,y) for d,x,y in heap]
+            ls.append(([x,y], (x**2 + y**2)**0.5))
+        return [i for i,v in sorted(ls, key=lambda x:x[1])[:k]]
+    
+    
