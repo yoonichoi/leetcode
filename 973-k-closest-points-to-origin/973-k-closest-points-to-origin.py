8ad97bb6854006c06ba21518fcbf97bd7f4e4a31
yoonichoi <111607447+yoonichoi@users.noreply.github.com>
2022-09-30 21:43:03 -0400

    Time: 668 ms (91.56%), Space: 20.9 MB (7.55%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        ls = []
+        for p in points:
+            x, y = p
+            ls.append(([x,y], (x**2 + y**2)**0.5))
+        return [i for i,v in sorted(ls, key=lambda x:x[1])[:k]]
