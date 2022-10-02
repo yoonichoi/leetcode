5aa9e25336edcb6d1ec0c17f7acbaa78ee49ee11
yoonichoi <111607447+yoonichoi@users.noreply.github.com>
2022-10-02 19:10:14 -0400

    Time: 773 ms (97.54%), Space: 14 MB (52.08%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -6,9 +6,8 @@ class Solution(object):
         :rtype: int
         """
         maxx = float('inf')
-        dp = [maxx] * (amount+1)
+        dp = [maxx] *(amount+1)
         dp[0] = 0
         for i in range(1, amount+1):
             dp[i] = min(dp[i-c] if i>=c else maxx for c in coins) + 1
         return -1 if dp[-1] == maxx else dp[-1]
-        
