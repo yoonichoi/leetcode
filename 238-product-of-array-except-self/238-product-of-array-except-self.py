d140e52240927a993b3a135e7d2775b2dc177ae7
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-10-03 16:50:15 -0400

    Time: 226 ms (82.32%), Space: 20.7 MB (85.48%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -4,13 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        res = [1] * len(nums)
-        prefix = 1
-        for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
-        for i in range(len(nums)-1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
+        n = len(nums)
+        ans, suffix = [1] * n, 1
+        for i in range(1,n):
+            ans[i] = ans[i-1] * nums[i-1]
+        for i in range(n-1, -1, -1):
+            ans[i] *= suffix
+            suffix *= nums[i]
+        return ans
