f000293db75457547dc282f0ebfc73ab994f0b6d
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-10-04 12:02:41 -0400

    Time: 268 ms (91.15%), Space: 29.1 MB (43.72%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,20 +4,17 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        if not grid:
-            return 0
-        
-        def dfs(grid,i,j):
-            if i<0 or i>=m or j<0 or j>=n or grid[i][j] != '1':
+        def dfs(x,y,grid):
+            if x < 0 or x>=m or y<0 or y>=n or grid[x][y] != '1':
                 return
-            grid[i][j] = '#'
-            for x, y in ((-1,0),(1,0),(0,-1),(0,1)):
-                dfs(grid, i+x, j+y)
-                
-        m, n, cnt = len(grid), len(grid[0]), 0
+            grid[x][y] = '#'
+            for dx, dy in ((0,1),(0,-1),(-1,0),(1,0)):
+                nx, ny = x+dx, y+dy
+                dfs(nx, ny, grid)
+        m, n, cnt= len(grid), len(grid[0]), 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1':
-                    dfs(grid, i, j)
+                    dfs(i,j,grid)
                     cnt += 1
         return cnt
