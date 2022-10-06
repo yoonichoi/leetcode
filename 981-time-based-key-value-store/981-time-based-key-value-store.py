69f6b78ce8e30156a09d69808e6926a17d183a45
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-10-06 11:17:26 -0400

    Time: 574 ms (97.94%), Space: 72.8 MB (68.46%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -1,74 +1,44 @@
 class TimeMap(object):
 
-#     def __init__(self):
-#         self.dic = collections.defaultdict(list)
-        
-
-#     def set(self, key, value, timestamp):
-#         """
-#         :type key: str
-#         :type value: str
-#         :type timestamp: int
-#         :rtype: None
-#         """
-#         self.dic[key].append((value, timestamp))
-        
-
-#     def get(self, key, timestamp):
-#         """
-#         :type key: str
-#         :type timestamp: int
-#         :rtype: str
-#         """
-#         if key in self.dic:
-#             li = self.dic[key]
-#             l, r = 0, len(li)-1
-#             if li[l][1] > timestamp:
-#                 return ""
-#             elif li[r][1] <= timestamp:
-#                 return li[r][0]
-#             else:
-#                 mid = l+(r-l) // 2
-#                 if li[mid][1] == timestamp:
-#                     return li[mid][0]
-#                 if li[mid][1] < timestamp:
-#                     l = mid +1
-#                 else:
-#                     r = mid -1
-#             return li[r][0]           
-#         return ""
-            
-        
     def __init__(self):
-        self._dic = defaultdict(list)
+        self.dic = collections.defaultdict(list)
+        
 
     def set(self, key, value, timestamp):
-        self._dic[key].append((value, timestamp,))
+        """
+        :type key: str
+        :type value: str
+        :type timestamp: int
+        :rtype: None
+        """
+        self.dic[key].append((value, timestamp))
+        
 
     def get(self, key, timestamp):
-        if key in self._dic:
-            li = self._dic[key]
-            l, r = 0, len(self._dic[key]) - 1
-            
+        """
+        :type key: str
+        :type timestamp: int
+        :rtype: str
+        """
+        if key in self.dic:
+            li = self.dic[key]
+            l, r = 0, len(li)-1
             if li[l][1] > timestamp:
                 return ""
             elif li[r][1] <= timestamp:
                 return li[r][0]
             else:
-                while l <= r:
-                    mid = l + (r - l) // 2
-
+                while l<= r:
+                    mid = l+(r-l) // 2
                     if li[mid][1] == timestamp:
                         return li[mid][0]
-
                     if li[mid][1] < timestamp:
-                        l = mid + 1
+                        l = mid +1
                     else:
-                        r = mid - 1
-
-                return li[r][0]
+                        r = mid -1
+                return li[r][0]           
         return ""
-
+        
         
 
 
