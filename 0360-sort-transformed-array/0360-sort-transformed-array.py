249009bbb74c1381cd5d11c1b24607e53ef711c4
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-10-23 20:28:23 -0400

    Time: 76 ms (60.96%), Space: 14 MB (8.66%) - LeetHub

--- 0360-sort-transformed-array/0360-sort-transformed-array.py
+++ 0360-sort-transformed-array/0360-sort-transformed-array.py
@@ -1,5 +1,4 @@
 class Solution:
     def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
-        # def transform(a,b,c,x):
-        #     return a*x**2 + b*x + c
+        
         return sorted(list(map(lambda x: a*x**2 + b*x + c, nums)))
