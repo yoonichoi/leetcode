931c25bbdddee567950a1d0fe8ebbb11737ea1db
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-10-23 20:34:17 -0400

    Time: 77 ms (59.85%), Space: 14 MB (8.66%) - LeetHub

--- 0360-sort-transformed-array/0360-sort-transformed-array.py
+++ 0360-sort-transformed-array/0360-sort-transformed-array.py
@@ -1,4 +1,26 @@
 class Solution:
     def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
-        
-        return sorted(list(map(lambda x: a*x**2 + b*x + c, nums)))
+        def quad(x):
+            return a*x**2 + b*x + c
+        n = len(nums)
+        index = 0 if a < 0 else n-1
+        l, r, res = 0, n-1, [0]*n
+        while l <= r:
+            lv, rv = quad(nums[l]), quad(nums[r])
+            if a>=0:
+                if lv > rv:
+                    res[index] = lv
+                    l += 1
+                else:
+                    res[index] = rv
+                    r -= 1
+                index -= 1
+            else:
+                if lv > rv:
+                    res[index] = rv
+                    r -= 1
+                else:
+                    res[index] = lv
+                    l += 1
+                index += 1
+        return res
