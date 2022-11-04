c1bc5467d2c2dced994c5bce641c17ccddb2611b
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-11-04 00:30:17 -0400

    Time: 70 ms (8.58%), Space: 13.9 MB (82.91%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -3,13 +3,13 @@ class Solution:
         m, n = len(matrix), len(matrix[0])
         top, bottom, left, right = 0, m-1, 0, n-1
         ans = []
-        while top <= bottom and left <= right:
+        while top <= bottom and left<=right:
             for col in range(left, right+1):
                 ans.append(matrix[top][col])
             top += 1
             for row in range(top, bottom+1):
                 ans.append(matrix[row][right])
-            right -= 1
+            right -=1
             for col in range(right, left-1, -1):
                 ans.append(matrix[bottom][col])
             bottom -= 1
