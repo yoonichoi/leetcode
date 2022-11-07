4b95d8ef6053eaa164b9029668cda488e4367bb3
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-11-07 08:15:54 -0500

    Time: 360 ms (9.85%), Space: 15.2 MB (36.39%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,14 +6,13 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def isMatch(p, q):
+        def isMatch(p,q):
             if not p and not q: return True
             elif not p or not q: return False
             else:
                 if p.val == q.val:
                     return isMatch(p.left, q.left) and isMatch(p.right, q.right)
-        if isMatch(root, subRoot):
-            return True
-        if not root: 
-            return False
+        if isMatch(root, subRoot): return True
+        if not root: return False
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        
