85e3a55bd687c237f0d94708689038c437945763
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-11-07 08:23:51 -0500

    Time: 37 ms (86.84%), Space: 13.8 MB (75.65%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -11,5 +11,3 @@ class Solution:
         else:
             if p.val == q.val:
                 return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-            else:
-                return False
