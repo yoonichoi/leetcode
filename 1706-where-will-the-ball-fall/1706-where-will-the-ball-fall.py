449ded2b9cdd37ce4818710cc2b98d650dd19f62
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-11-08 18:44:23 -0500

    Time: 211 ms (87.93%), Space: 14.6 MB (23.28%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -2,13 +2,15 @@ class Solution:
     def findBall(self, grid: List[List[int]]) -> List[int]:
         m, n = len(grid), len(grid[0])
         def drop(r,c):
-            if r==m: return c
-            elif grid[r][c] == 1 and c+1<n and grid[r][c+1] == 1:
-                return drop(r+1,c+1)
-            elif grid[r][c] == -1 and c-1 >= 0 and grid[r][c-1] == -1:
+            if r == m: return c
+            elif grid[r][c] == 1 and c+1<n and grid[r][c+1]==1:
+                return drop(r+1, c+1)
+            elif grid[r][c] == -1 and c-1>=0 and grid[r][c-1]==-1:
                 return drop(r+1, c-1)
             else: return -1
-        return [drop(0,c) for c in range(n)]
+        return [drop(0, c) for c in range(n)]
         
         
- 
+        
+        
+       
