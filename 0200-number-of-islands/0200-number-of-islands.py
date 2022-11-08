11bf565ad3bd20bc2e91c7944fa26f3dc34addc4
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-11-08 18:52:54 -0500

    Time: 1150 ms (9.41%), Space: 16.4 MB (81.94%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,14 +1,14 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid: return 0
         def dfs(i,j):
-            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]!='1':
+            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
                 return
             grid[i][j] = '#'
-            dfs(i+1,j)
-            dfs(i-1,j)
-            dfs(i,j+1)
-            dfs(i,j-1)
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+        if not grid: return 0
         cnt = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
@@ -16,4 +16,3 @@ class Solution:
                     dfs(i,j)
                     cnt += 1
         return cnt
-        
