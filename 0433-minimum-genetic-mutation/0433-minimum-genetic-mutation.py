995ab4bb5c4c698ede1307f66b1d434166212677
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-11-10 00:06:49 -0500

    Time: 35 ms (88.59%), Space: 13.9 MB (86.77%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,7 +1,7 @@
 class Solution:
     def minMutation(self, start: str, end: str, bank: List[str]) -> int:
         def onechange(a,b):
-            return sum([1 for i in range(len(a)) if a[i] != b[i]]) == 1
+            return sum([1 for i in range(len(a)) if a[i]!=b[i]])==1
         q = collections.deque([(start, 0)])
         seen = {start}
         while q:
@@ -9,10 +9,8 @@ class Solution:
             if gene == end:
                 return cnt
             for g in bank:
-                if g not in seen and onechange(g, gene):
-                    seen.add(g)
+                if onechange(g, gene) and g not in seen:
                     q.append((g, cnt+1))
+                    seen.add(g)
         return -1
         
-        
-     
