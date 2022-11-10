66ec96932dca015ef4421b69f7e8cce73f40be41
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-11-09 23:59:39 -0500

    Time: 5514 ms (54.29%), Space: 14 MB (13.35%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,23 +1,17 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m, n = len(board), len(board[0])
-        visited = set()
-        res = False
-        
-        def dfs(i,j,visited,word,index):
-            if index == len(word): return True
-            if i<0 or j<0 or i>=m or j>=n or (i,j) in visited or word[index]!=board[i][j]:
-                return False
-            visited.add((i,j))
-            res = (dfs(i+1,j,visited,word,index+1) or
-                dfs(i-1,j,visited,word,index+1) or
-                dfs(i,j+1,visited,word,index+1) or
-                dfs(i,j-1,visited,word,index+1))
-            visited.remove((i,j))
+        def check(board, i, j, word):
+            if len(word) == 0: return True
+            if i<0 or j<0 or i>=len(board) or j>=len(board[0]) or word[0] != board[i][j]: return False
+            tmp = board[i][j]
+            board[i][j] = '#'
+            res = check(board, i+1, j, word[1:]) or check(board, i-1, j, word[1:]) or check(board, i, j+1, word[1:]) or check(board, i, j-1, word[1:])
+            board[i][j] = tmp
             return res
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == word[0]:
-                    if dfs(i,j,visited,word,0):
-                        return True
+        if not board: return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if check(board, i, j, word):
+                    return True
         return False
+        
