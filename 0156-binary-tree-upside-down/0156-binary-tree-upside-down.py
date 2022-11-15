393023eeaf88784ce826a9fb22440dab87ec32fb
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-11-15 17:37:19 -0500

    Time: 34 ms (93.62%), Space: 13.9 MB (63.76%) - LeetHub

--- 0156-binary-tree-upside-down/0156-binary-tree-upside-down.py
+++ 0156-binary-tree-upside-down/0156-binary-tree-upside-down.py
@@ -9,8 +9,7 @@ class Solution:
         if not root or (not root.left and not root.right):
             return root
         left = self.upsideDownBinaryTree(root.left)
-        root.left.left = root.right
         root.left.right = root
-        root.left = None
-        root.right = None
+        root.left.left = root.right
+        root.left, root.right = None, None
         return left
