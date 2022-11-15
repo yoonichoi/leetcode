5a1d0c16f331b0e70683e48a8f134ac9268eed1a
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-11-14 23:53:42 -0500

    Time: 220 ms (50.51%), Space: 22 MB (28.31%) - LeetHub

--- 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.py
+++ 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.py
@@ -2,9 +2,8 @@ class WordDistance:
 
     def __init__(self, wordsDict: List[str]):
         self.d = collections.defaultdict(list)
-        for i,w in enumerate(wordsDict):
-            self.d[w].append(i)
-        
+        for i, word in enumerate(wordsDict):
+            self.d[word].append(i)
 
     def shortest(self, word1: str, word2: str) -> int:
         return min(abs(n1-n2) for n1 in self.d[word1] for n2 in self.d[word2])
