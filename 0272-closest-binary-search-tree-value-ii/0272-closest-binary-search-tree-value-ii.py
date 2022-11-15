a9543ca4ea853aa49aa42d73bb96ebab0b176ffc
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-11-15 14:02:54 -0500

    Time: 131 ms (15.11%), Space: 16.7 MB (28.78%) - LeetHub

---
+++ 0272-closest-binary-search-tree-value-ii/0272-closest-binary-search-tree-value-ii.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import heapq
+class Solution:
+    def closestKValues(self, root: Optional[TreeNode], target: float, k: int) -> List[int]:
+        def dfs(root, target, heap):
+            if not root: return
+            dfs(root.left, target, heap)
+            heapq.heappush(heap, (abs(root.val-target), root.val))
+            dfs(root.right, target, heap)
+        heap = []
+        dfs(root,target,heap)
+        res = []
+        for _ in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
