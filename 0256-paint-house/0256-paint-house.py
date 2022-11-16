ae6046d66094db0ed09af8fe3e4a89f5a40845b3
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-11-16 14:29:22 -0500

    Time: 115 ms (67.37%), Space: 14 MB (56.73%) - LeetHub

--- 0256-paint-house/0256-paint-house.py
+++ 0256-paint-house/0256-paint-house.py
@@ -1,6 +1,6 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-        r,b,g = 0,0,0
+        r, b, g = 0, 0, 0
         for cr, cb, cg in costs:
-            r,b,g = min(b,g)+cr, min(r,g)+cb, min(r,b)+cg
+            r, b, g = min(b,g) + cr, min(r,g)+cb, min(r,b) + cg
         return min(r,b,g)
