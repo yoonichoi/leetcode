efe9aec91c8b7cb71988f5280e6d1a159e979046
Yooni (Jungyoon) Choi <111607447+yoonichoi@users.noreply.github.com>
2022-11-16 14:46:59 -0500

    Time: 57 ms (78.50%), Space: 13.9 MB (50.10%) - LeetHub

--- 0360-sort-transformed-array/0360-sort-transformed-array.py
+++ 0360-sort-transformed-array/0360-sort-transformed-array.py
@@ -3,24 +3,25 @@ class Solution:
         def quad(x):
             return a*x**2 + b*x + c
         n = len(nums)
-        l, r, res = 0, n-1, [0] * n
-        idx = 0 if a<0 else n-1
+        res = [0] * n
+        l, r = 0, n-1
+        idx = 0 if a < 0 else n-1
         while l<=r:
             lv, rv = quad(nums[l]), quad(nums[r])
-            if a >= 0 :
-                if lv > rv:
+            if a < 0:
+                if lv < rv:
                     res[idx] = lv
                     l += 1
                 else:
                     res[idx] = rv
                     r -= 1
-                idx -= 1
-            else:
-                if lv > rv:
-                    res[idx] = rv
-                    r -= 1
-                else:
-                    res[idx] = lv
-                    l += 1
                 idx += 1
+            else:
+                if lv < rv:
+                    res[idx] = rv
+                    r -= 1
+                else:
+                    res[idx] = lv
+                    l += 1
+                idx -= 1
         return res
